5b41ad0c57339d84dfeee2dbd9654a2e218b8474
Jonas Augusto <jonasaugusto99@gmail.com>
2022-08-15 20:06:37 -0300

    :sparkles: feat: Add About Component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 import './about.css';
+import ME from '../../assets/about-me.jpeg'
+import {FaAward} from 'react-icons/fa'
+import {MdWork} from 'react-icons/md'
+import {BsFillFolderFill} from 'react-icons/bs'
+
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>Sobre mim</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt="About" />
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experiência</h5>
+              <small>Junior</small>
+            </article>
+
+            <article className='about__card'>
+              <MdWork className='about__icon' />
+              <h5>Work</h5>
+              <small>Intelligence Kapital</small>
+            </article>
+
+            <article className='about__card'>
+              <BsFillFolderFill className='about__icon' />
+              <h5>Projetos</h5>
+              <small>+40 Projetos</small>
+            </article>
+          </div>
+
+          <p>
+            Sou um apaixonado pela área de programação e tecnologia, por toda a gama de oportunidades de fazer acontecer que ela proporciona e pela a chance de poder impactar positivamente a sociedade.
+            <br />Ingressei no curso de Engenharia de Software na UniCV e estou me especializando em Backend pela Gama Academy.
+            <br />Gosto de estudar e aprender idiomas. Tenho fluência em Inglês e Francês e estudo italiano e japonês. <br />Sou uma pessoa amante do conhecimento, gosto de novos desafios, aprender novos assuntos, estudar idiomas, conhecer novas culturas e busco impactar positivamente a sociedade como um todo.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Entre em Contato</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,89 @@
 import React from 'react'
 import './experience.css'
+import {BsFillCheckCircleFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>Quais Habilidades Eu Tenho</h5>
+      <h2>Minha Experiência</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Desenvolvimento Frontend</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced Gama Academy</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced Gama Academy</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Experienced Gama Academy</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Experienced Code Academy</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONTEND */}
+        <div className='experience__backend'>
+          <h3>Desenvolvimento Backend</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+                <BsFillCheckCircleFill className='experience__details-icon' />
+                <div>
+                  <h4>Node JS</h4>
+                  <small className='text-light'>Experienced Gama Academy</small>
+                </div>
+            </article>
+
+            <article className='experience__details'>
+                <BsFillCheckCircleFill className='experience__details-icon' />
+                <div>
+                  <h4>C#</h4>
+                  <small className='text-light'>Experienced Code Academy</small>
+                </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>Java</h4>
+                <small className='text-light'>Experienced Alura</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillCheckCircleFill className='experience__details-icon' />
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Experienced Gama Academy</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
