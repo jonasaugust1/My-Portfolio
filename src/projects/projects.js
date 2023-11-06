d22d2ef2c800ae8e373c4d9b2ba04322b1032318
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-11-06 17:08:04 -0300

    Updating CV and adding new project to portfolio

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -5,14 +5,22 @@ export const projects = [
     description: 'I created a powerful React Native mobile app for an Agricultural Aviation company, enabling streamlined operations, improved safety practices, and easy access to crucial information. The app allows users to generate detailed reports, access client and aircraft data, and obtain certifications. With a user-friendly interface and optimized performance, it enhances workflow processes and empowers employees to excel in their roles.',
     image: '',
     technologies: ['React Native'],
-    github: 'https://github.com/jonasaugust1/CdeSafety-images'
+    github: ''
   },
   {
     id: 1,
     title: 'Two Factor Authentication',
     description: 'I successfully implemented two-factor authentication in the Social Security area of Sinqia\'s authentication portal. This enhanced security measures, reduced unauthorized access risks, and showcased my commitment to delivering secure solutions. The feature provides an additional layer of defense, protecting sensitive data and building user trust.',
     image: '',
-    technologies: ['C#', 'Visual Basic', 'Xamarin'],
+    technologies: ['C#', 'Visual Basic', 'Xamarin', '.NET Framework'],
+    github: ''
+  },
+  {
+    id: 2,
+    title: 'API Comparer',
+    description: 'I collaborated with my team to develop an API comparer for Sinqia\'s internal use. This tool enables users to compare two versions of APIs without worrying about exposing confidential files.',
+    image: '',
+    technologies: ['Razor Pages', 'C#', '.NET Core'],
     github: ''
   },
 ];
