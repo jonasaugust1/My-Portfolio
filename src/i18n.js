9600f75afa0bfeb061ab97a41f4bcc7f84a0162f
Jonas Augusto <jonasaugusto99@gmail.com>
2026-02-24 17:20:58 -0300

    feat: Translating contact component

--- src/i18n.js
+++ src/i18n.js
@@ -17,14 +17,29 @@ i18n
           "scroll-down": "Scroll Down",
           "get-to-know-me": "Get to Know Me",
           "about-me": "About Me",
-                "experience": "Experience",
+          "experience": "Experience",
           "work": "Work",
           "projects": "Projects",
           "years": "4 years",
           "projects-count": "+90 Projects",
 
           "about-description": "From an early age, I have been fascinated by the ability of technology to transform the world around us. I chose to become a software developer because I believe in the power of creating solutions that positively impact the lives of millions of people. The possibility of streamlining daily life and accelerating processes motivates me with every line of code I write.\n\nMy journey in software development is driven by the desire to break patterns and discover new ways of building things. Contributing to innovative projects fuels my passion, and I am constantly seeking opportunities to apply my skills to initiatives that truly make a difference.\n\nFeel free to reach out to discuss collaboration opportunities, networking, or simply to exchange ideas about the exciting world of software development. Let’s build something amazing together!",
+          "contact-section-title-small": "Contact",
+          "contact-section-title": "Send a Message",
 
+          "email": "Email",
+          "whatsapp": "WhatsApp",
+
+          "send-email": "Send an email",
+          "get-in-touch": "Get in touch",
+
+          "full-name": "Full Name",
+          "your-email": "Your Email",
+          "your-message": "Your Message",
+
+          "invalid-email": "Please enter a valid email address.",
+          "message-success": "Message sent successfully.",
+          "send-message-btn": "Send a message"
         }
       },
       pt: {
@@ -44,6 +59,22 @@ i18n
           "projects-count": "+90 Projetos",
 
           "about-description": "Desde cedo, sempre fui fascinado pela capacidade da tecnologia de transformar o mundo ao nosso redor. Escolhi me tornar desenvolvedor de software porque acredito no poder de criar soluções que impactam positivamente a vida de milhões de pessoas. A possibilidade de otimizar o dia a dia e acelerar processos me motiva a cada linha de código que escrevo.\n\nMinha jornada no desenvolvimento é guiada pelo desejo de quebrar padrões e encontrar novas formas de construir soluções. Contribuir para projetos inovadores é o que me impulsiona, e estou sempre em busca de oportunidades para aplicar minhas habilidades em iniciativas que realmente façam a diferença.\n\nSinta-se à vontade para entrar em contato para discutirmos oportunidades de colaboração, networking ou simplesmente trocar ideias sobre o fascinante mundo do desenvolvimento de software. Vamos construir algo incrível juntos!",
+          "contact-section-title-small": "Contato",
+          "contact-section-title": "Envie uma Mensagem",
+
+          "email": "Email",
+          "whatsapp": "WhatsApp",
+
+          "send-email": "Enviar email",
+          "get-in-touch": "Entrar em contato",
+
+          "full-name": "Nome Completo",
+          "your-email": "Seu Email",
+          "your-message": "Sua Mensagem",
+
+          "invalid-email": "Por favor, insira um email válido.",
+          "message-success": "Mensagem enviada com sucesso.",
+          "send-message-btn": "Enviar mensagem"
         }
       }
     },
