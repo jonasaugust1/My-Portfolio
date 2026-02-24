755ea46880e9858eba4af7b5774e32ecc9f66b02
Jonas Augusto <jonasaugusto99@gmail.com>
2026-02-24 19:11:35 -0300

    config: updating sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,22 +1,22 @@
-import { SitemapStream, streamToPromise } from 'sitemap';
-import { createWriteStream } from 'fs';
+import { SitemapStream, streamToPromise } from 'sitemap'
+import { createWriteStream } from 'fs'
 
 const sitemap = new SitemapStream({
-  hostname: 'https://jonasaugusto.com',
-});
+  hostname: 'https://www.jonasaugusto.com',
+})
 
-const writeStream = createWriteStream('./public/sitemap.xml');
+const writeStream = createWriteStream('./public/sitemap.xml')
 
-sitemap.pipe(writeStream);
+sitemap.pipe(writeStream)
 
-sitemap.write({ url: '/', changefreq: 'monthly', priority: 1.0 });
-sitemap.write({ url: '/#about', changefreq: 'monthly', priority: 0.8 });
-sitemap.write({ url: '/#experience', changefreq: 'monthly', priority: 0.8 });
-sitemap.write({ url: '/#portfolio', changefreq: 'monthly', priority: 0.8 });
-sitemap.write({ url: '/#contact', changefreq: 'monthly', priority: 0.8 });
+sitemap.write({
+  url: '/',
+  changefreq: 'monthly',
+  priority: 1.0,
+})
 
-sitemap.end();
+sitemap.end()
 
 streamToPromise(sitemap).then(() => {
-  console.log('Sitemap generated successfully.');
-});
+  console.log('Sitemap generated successfully.')
+})
