70340d80ca2fb02c25e7321a63669bf10575077b
Jonas Augusto <jonasaugusto99@gmail.com>
2026-02-26 15:15:06 -0300

    config: Adding intern links on sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -9,11 +9,11 @@ const writeStream = createWriteStream('./public/sitemap.xml')
 
 sitemap.pipe(writeStream)
 
-sitemap.write({
-  url: '/',
-  changefreq: 'monthly',
-  priority: 1.0,
-})
+sitemap.write({ url: '/', changefreq: 'monthly', priority: 1.0 });
+sitemap.write({ url: '/#about', changefreq: 'monthly', priority: 0.8 });
+sitemap.write({ url: '/#experience', changefreq: 'monthly', priority: 0.8 });
+sitemap.write({ url: '/#portfolio', changefreq: 'monthly', priority: 0.8 });
+sitemap.write({ url: '/#contact', changefreq: 'monthly', priority: 0.8 });
 
 sitemap.end()
 
